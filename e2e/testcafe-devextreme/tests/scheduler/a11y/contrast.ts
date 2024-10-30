4b56f908727ca306420d000abc067c5b6b3e1611
tongsonbarbs <tongsonbarbs@gmail.com>
2024-10-30 23:49:50 +0800

    remove debug

--- e2e/testcafe-devextreme/tests/scheduler/a11y/contrast.ts
+++ e2e/testcafe-devextreme/tests/scheduler/a11y/contrast.ts
@@ -19,7 +19,7 @@ fixture.disablePageReloads`a11y - contrast`
   test(`Scheduler a11y: Insufficient contrast of day numbers in the MonthView (${theme})`, async (t) => {
     const scheduler = new Scheduler('#container');
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
-    await t.debug();
+
     await t
       .expect(takeScreenshot(`month_day_number_contrast-${theme}`, scheduler.element))
       .ok()
