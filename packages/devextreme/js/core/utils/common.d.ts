5799bbddddf8debfccc3db0e3ccb2c294328be0d
tongsonbarbs <tongsonbarbs@gmail.com>
2024-11-19 16:21:51 +0800

    add export function declaration

--- packages/devextreme/js/core/utils/common.d.ts
+++ packages/devextreme/js/core/utils/common.d.ts
@@ -7,6 +7,8 @@ export interface EqualByValueOptions {
 
 export function noop(): void;
 
+export function getKeyHash(key: any): any;
+
 export function deferRender<T>(func: () => T, deferred?: DeferredObj<T>): T | Promise<T> | DeferredObj<T>;
 
 export function ensureDefined<T>(value: T, defaultValue: T): NonNullable<T>;
