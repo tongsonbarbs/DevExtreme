ab6a68652161756196be437baa44394f737e8091
Roman Semenov <roman.semenov@devexpress.com>
2024-11-27 22:20:26 +0400

    storybook fixes

--- apps/react-storybook/stories/card_view/CardView.stories.tsx
+++ apps/react-storybook/stories/card_view/CardView.stories.tsx
@@ -1,34 +1,54 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import CardView from 'devextreme-react/card-view';
-import { items } from './data';
+import dxCardView from "devextreme/ui/card_view";
+import { wrapDxWithReact } from "../utils";
+import { items, store } from "./data";
+
+const CardView = wrapDxWithReact(dxCardView);
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta<typeof CardView> = {
-  title: 'Grids/CardView',
+  title: "Grids/CardView",
   component: CardView,
+  argTypes: {
+    dataSource: {
+      control: false,
+    }
+  }
 };
 
 export default meta;
-
 
 type Story = StoryObj<typeof CardView>;
 
 export const DefaultMode: Story = {
   args: {
-    dataSource: items,
+    dataSource: store,
+    width: "100%",
+    // width: 750,
+    height: 500,
+    keyExpr: "OrderNumber",
+    cardsPerRow: "auto",
     paging: {
-      pageIndex: 0,
-      pageSize: 5,
+      pageSize: 12,
     },
+    cardMinWidth: 250,
+    cardMaxWidth: 350,
     columns: [
       {
-        dataField: 'column1',
+        dataField: "OrderNumber",
         alignment: 'right',
+        dataType: "number",
       },
       {
-        dataField: 'column2',
-      }
-    ]
+        dataField: "OrderDate",
+        visible: false,
+      },
+      "StoreCity",
+      "StoreState",
+      "Employee",
+      "SaleAmount",
+    ],
+    filterPanel: { visible: true },
   },
 };
