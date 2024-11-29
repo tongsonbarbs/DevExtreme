06651cd1b606f1fa5790a01f0402641e2f9f9116
Joshua Barbosa <44610149+tongsonbarbs@users.noreply.github.com>
2024-11-29 15:50:49 +0800

    Scheduler - The CurrentView property doesn't accept custom view names (T1256198) (#28443)

--- packages/devextreme/ts/dx.all.d.ts
+++ packages/devextreme/ts/dx.all.d.ts
@@ -23931,7 +23931,7 @@ declare module DevExpress.ui {
     /**
      * [descr:dxSchedulerOptions.currentView]
      */
-    currentView?: DevExpress.ui.dxScheduler.ViewType;
+    currentView?: DevExpress.ui.dxScheduler.ViewType | string;
     /**
      * [descr:dxSchedulerOptions.customizeDateNavigatorText]
      */
